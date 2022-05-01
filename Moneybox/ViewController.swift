fd8f78aca721c21fc4e032c587c73741fb761c00
Fernando Perez <fernando.perez@payclip.com>
2022-04-30 19:09:38 -0500

    Initial Commit

---
+++ Moneybox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moneybox
+//
+//  Created by Fernando  Perez on 30/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
